0fea5ee8f26739b0d453b98dd6b33b64db68845c
Riley Raschke <riley@rrappsdev.com>
2020-01-21 23:41:52 -0600

    Add template engine, template, remote resources/libs, static files

--- app.js
+++ app.js
@@ -1,12 +1,68 @@
 
+/**
+ * Requires/imports
+ */
 const express = require('express')
 const config  = require('config')
+const path = require('path')
 
+/**
+ * Server Side App init
+ */
 const app = express()
 const port = config.get('server.port')
 
+/**
+ * Doc: https://ejs.co/#docs
+ * This part picks out syntax/features for stuff under ./views/*
+ * ** THERE ARE TOO MANY WAYS TO SKIN THIS CAT **
+ */
+app.set('view engine', 'ejs')
+
+/**
+ * Static content (css,js,etc)
+ */
+app.use("/static/", express.static(path.join(__dirname, 'static')))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  let contactGroups = [
+    {
+      name: 'Applications',
+      contacts: [
+        {name: 'Riley', ext: '0101'},
+        {name: 'OtherPerson', ext: '0102'},
+      ]
+    },
+    {
+      name: 'Field Services',
+      contacts: [
+        {name: 'AnotherPer', ext: '1010'},
+        {name: 'RamJam', ext: '1011'},
+      ]
+    },
+    {
+      name: 'Server',
+      contacts: [
+        {name: 'Demo Uno', ext: '0103'},
+        {name: 'Demo Dos', ext: '0104'},
+      ],
+    },
+    {
+      name: 'Networking',
+      contacts: [
+        {name: 'Too Many', ext: '0105'},
+        {name: 'Json is Cool', ext: '0106'},
+      ]
+    },
+    {
+      name: 'Security',
+      contacts: [
+        {name: 'Contact 4', ext: '0107'},
+        {name: 'Contact 5 ', ext: '0000'},
+      ]
+    },
+  ]
+  res.render('home', { contactGroups: contactGroups });
 });
 
 app.listen(port, () => console.log(`Express server started on port: ${port}!`))

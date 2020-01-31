cae0e622ec0ba921d60df8491dda8b7535e60a1d
Riley Raschke <riley@rrappsdev.com>
2020-01-30 19:26:12 -0600

    Move inline object from route into config

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  * Requires/imports
  */
 const express = require('express')
+const partials = require('express-partials')
 const config  = require('config')
 const path = require('path')
 
@@ -17,6 +18,7 @@ const port = config.get('server.port')
  * This part picks out syntax/features for stuff under ./views/*
  * ** THERE ARE TOO MANY WAYS TO SKIN THIS CAT **
  */
+app.use(partials())
 app.set('view engine', 'ejs')
 
 /**
@@ -25,49 +27,8 @@ app.set('view engine', 'ejs')
 app.use("/static/", express.static(path.join(__dirname, 'static')))
 
 app.get('/', (req, res) => {
-  /**
-   * No.. this inline object shouldn't live in the middle of our route control code...
-   * Ideally, it would come from 'model' code.. and model code will be retrieving it from somewhere else (config/db/directory)
-   * Probably config in this special scenario!
-   */
-  let contactGroups = [
-    {
-      name: 'Applications',
-      contacts: [
-        {name: 'Riley', ext: '0101'},
-        {name: 'OtherPerson', ext: '0102'},
-      ]
-    },
-    {
-      name: 'Field Services',
-      contacts: [
-        {name: 'AnotherPer', ext: '1010'},
-        {name: 'RamJam', ext: '1011'},
-      ]
-    },
-    {
-      name: 'Server',
-      contacts: [
-        {name: 'Demo Uno', ext: '0103'},
-        {name: 'Demo Dos', ext: '0104'},
-      ],
-    },
-    {
-      name: 'Networking',
-      contacts: [
-        {name: 'Too Many', ext: '0105'},
-        {name: 'Json is Cool', ext: '0106'},
-      ]
-    },
-    {
-      name: 'Security',
-      contacts: [
-        {name: 'Contact 4', ext: '0107'},
-        {name: 'Contact 5 ', ext: '0000'},
-      ]
-    },
-  ]
-  res.render('home', { contactGroups: contactGroups });
+  let contactGroups =  config.contactGroups
+  res.render('home', { contactGroups: contactGroups })
 });
 
 app.listen(port, () => console.log(`Express server started on port: ${port}!`))
